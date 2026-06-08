49729c08b976775a0aab5a660e3cad0e2288014a
arthurscalabre <arthscal@gmail.com>
2026-06-08 10:53:25 +0200

    résolution bug pour les parts dans index

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from calculer_revenu_net import *
 
 revenu_brut = float(input("Entrez votre revenu : "))
 revenu_net = calculer_revenu_net(revenu_brut)
-revenu_imposable = revenu_net/calcul_parts()
-impot_base = formule_ir(revenu_net) * calcul_parts
-
+nb_parts = calcul_parts()
+revenu_imposable = revenu_net/nb_parts
+impot_base = formule_ir(revenu_net) * nb_parts
 print("impot sur le revenu =", impot_base)
