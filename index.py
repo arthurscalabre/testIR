2807894999b4d9cef0c07ac6ed4bf83fa434158c
arthurscalabre <arthscal@gmail.com>
2026-06-08 11:12:45 +0200

    ajout du calcul du revenu imposable par le quotient familial

--- index.py
+++ index.py
@@ -2,11 +2,12 @@ from calculs_parts import *
 from formule_ir import *
 from calculer_revenu_net import *
 from calcul_decote import *
+from quotient_familial import *
 
 revenu_brut = float(input("Entrez votre revenu : "))
 revenu_net = calculer_revenu_net(revenu_brut)
 nb_parts = calcul_parts()
-revenu_imposable = revenu_net/nb_parts
+revenu_imposable = quotient_familial(revenu_net, nb_parts)
 impot_brut = formule_ir(revenu_net) * nb_parts
 decote = calcul_decote(impot_brut, nb_parts)
 impot = impot_brut - decote
---
+++ quotient_familial.py
@@ -0,0 +1,3 @@
+def quotient_familial (revenu_net, nb_parts):
+    revenu_imposable = revenu_net/nb_parts
+    return revenu_imposable
