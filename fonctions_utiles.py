baf8153bbefd23dd6ee7d745568ee73033df4e91
arthurscalabre <arthscal@gmail.com>
2026-06-08 16:59:10 +0200

    créations d'un fichier "parameters" de constante

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,6 +1,7 @@
+from parameters import *
+
 def calculer_revenu_net (revenu_brut) :
-    abbatement_forfaitaire = 0.9
-    revenu_net = revenu_brut*abbatement_forfaitaire
+    revenu_net = revenu_brut*ABATTEMENT_FORFAITAIRE
     return revenu_net
 
 def calcul_parts() : 
@@ -17,19 +18,7 @@ def quotient_familial (revenu_net, nb_parts):
     return revenu_imposable
 
 def formule_ir(revenu_net):
-    value_type = float
     impot = 0
-
-    P0 = 0
-    P1 = 0.11
-    P2 = 0.30
-    P3 = 0.41
-    P4 = 0.45
-
-    T1 = 11600
-    T2 = 29579
-    T3 = 84577
-    T4 = 181917
 
     if revenu_net > T1:
         if revenu_net > T2:
@@ -55,9 +44,9 @@ def formule_ir(revenu_net):
 
 def calcul_decote(impot_brut, nb_parts):
     if nb_parts <= 1:  
-        decote = 897 - (0.4525 * impot_brut)
+        decote = SEUIL_DECOTE_SEUL - (TAUX_DECOTE * impot_brut)
     else:              
-        decote = 1483 - (0.4525 * impot_brut)
+        decote = SEUIL_DECOTE_COUPLE - (TAUX_DECOTE * impot_brut)
     # La décote ne peut pas être négative
     decote = max(0, decote)
     return decote
