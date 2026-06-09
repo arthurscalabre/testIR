9ffb18588ed3727b198d938c0c5cefdcbba899db
arthurscalabre <arthscal@gmail.com>
2026-06-09 10:32:56 +0200

    calcul des parts par adultes et enfants

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -5,6 +5,10 @@ def calculer_revenu_net (revenu_brut) :
     revenu_net = revenu_brut*abbatement_forfaitaire
     return revenu_net
 
+def calcul_parts (adultes, enfants) : 
+    parts = np.where (enfants<3, adultes + enfants*0.5, adultes + enfants - 1) #moins 1 car les 2 premiers enfants compte pour 1 (0.5 chacun)
+    return parts
+
 def quotient_familial (revenu_net, parts) :
     revenu_imposable = revenu_net / parts
     return revenu_imposable
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import numpy as np
 from fonctions_utiles import *
 
 def calcul_ir(revenu_brut) : #on part du principe qu'on a une seule part
+    parts = np.array(calcul_parts(adultes, enfants))
     revenu_net = np.array(calculer_revenu_net(revenu_brut))
     revenu_imposable = np.array(quotient_familial(revenu_net, parts))
     impot_par_parts = np.array(formule_ir(revenu_imposable))
@@ -10,6 +11,7 @@ def calcul_ir(revenu_brut) : #on part du principe qu'on a une seule part
     impot = np.array(enleve_decote(droits_simples, decote))
     return(impot)
 
-revenu_brut = np.array([20000, 70000, 120000])
-parts = np.array([1, 1, 2])
+revenu_brut = np.array([200000, 70000, 120000])
+adultes = np.array([2, 2, 2])
+enfants = np.array([1, 4, 1])
 print (calcul_ir(revenu_brut))
