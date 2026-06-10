f3311ea0cb1c9415e2ee4fa95f8dd3fc1670d23d
arthurscalabre <arthscal@gmail.com>
2026-06-10 11:38:05 +0200

    ajout du plafonnement sur les parts des enfants (enleve calcul de l'index mais orga brouillon

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,13 +1,13 @@
 import numpy as np
 from parameters import *
 
-def calculer_revenu_net (revenu_brut) :
-    revenu_net = revenu_brut*ABATTEMENT_FORFAITAIRE
-    return revenu_net
-
 def calcul_parts (adultes, enfants) : 
     parts = np.where (enfants<=2, adultes*NB_PARTS_ADULTE + enfants*NB_PARTS_ENFANTS_1ou2, adultes + enfants*NB_PARTS_ENFANTS3 - 2*NB_PARTS_ENFANTS_1ou2) #moins 1 car les 2 premiers enfants compte pour 1 (0.5 chacun)
     return parts
+
+def calculer_revenu_net (revenu_brut) :
+    revenu_net = revenu_brut*ABATTEMENT_FORFAITAIRE
+    return revenu_net
 
 def quotient_familial (revenu_net, parts) :
     revenu_imposable = revenu_net / parts
@@ -43,3 +43,33 @@ def appliquer_minimum(impot):
 def calcul_taux_imposition(impot,revenu_brut) :
     taux_imposition = impot/revenu_brut
     return taux_imposition
+
+def calcul_ir_sans_enfants(revenu_brut, adultes, enfants) : #on part du principe qu'on a une seule part
+    parts = np.array(calcul_parts(adultes, enfants))
+    revenu_net = calculer_revenu_net(revenu_brut)
+    revenu_imposable = quotient_familial(revenu_net, parts)
+    impot_par_parts = formule_ir(revenu_imposable)
+    droits_simples = calcul_droits_simples (impot_par_parts, parts)
+    decote = calcul_decote(droits_simples)
+    impot = enleve_decote(droits_simples, decote)
+    impot_final = appliquer_minimum(impot)
+    return(impot_final)
+
+def plafonnement_QF(impot, parts, revenu_brut, adultes) :
+    parts_enfants = parts - adultes * NB_PARTS_ADULTE
+    plafonnement = PLAFOND_PAR_PART * parts_enfants
+    reduction = calcul_ir_sans_enfants(revenu_brut,adultes,0) - impot #calcul_ir(revenu_brut,adultes,0) = calcule ir sans part enfants
+    impot_plafonne = calcul_ir_sans_enfants(revenu_brut,adultes,0) - np.minimum(reduction, plafonnement)
+    return impot_plafonne
+
+def calcul_ir(revenu_brut, adultes, enfants) : #on part du principe qu'on a une seule part
+    parts = np.array(calcul_parts(adultes, enfants))
+    revenu_net = calculer_revenu_net(revenu_brut)
+    revenu_imposable = quotient_familial(revenu_net, parts)
+    impot_par_parts = formule_ir(revenu_imposable)
+    droits_simples = calcul_droits_simples (impot_par_parts, parts)
+    impot = np.where(enfants > 0, plafonnement_QF(droits_simples, parts, revenu_brut, adultes), droits_simples)
+    decote = calcul_decote(impot)
+    impot = enleve_decote(impot, decote)
+    impot_final = appliquer_minimum(impot)
+    return(impot_final)
--- index.py
+++ index.py
@@ -1,23 +1,13 @@
 import numpy as np
 from fonctions_utiles import *
 
-def calcul_ir(revenu_brut, adultes, enfants) : #on part du principe qu'on a une seule part
-    parts = calcul_parts(adultes, enfants)
-    revenu_net = calculer_revenu_net(revenu_brut)
-    revenu_imposable = quotient_familial(revenu_net, parts)
-    impot_par_parts = formule_ir(revenu_imposable)
-    droits_simples = calcul_droits_simples (impot_par_parts, parts)
-    decote = calcul_decote(droits_simples)
-    impot = enleve_decote(droits_simples, decote)
-    impot_final = appliquer_minimum(impot)
-    return(impot_final)
-
-revenu_brut = np.array([19500, 20000, 19800])
-adultes = np.array([1, 1, 1])
-enfants = np.array([0, 0, 0])
+revenu_brut = np.array([19500, 20000, 74436])
+adultes = np.array([1, 1, 2])
+enfants = np.array([0, 0, 2])
 
 impot = np.array(calcul_ir(revenu_brut, adultes, enfants))
 print("impot sur le revenu =", impot)
 
-taux_imposition = np.array(calcul_taux_imposition(impot,revenu_brut))
-print("taux d'imposition =", np.round(taux_imposition * 100, 1), "%")  #en %
+#taux_imposition = np.array(calcul_taux_imposition(impot,revenu_brut))
+#print("taux d'imposition =", np.round(taux_imposition * 100, 1), "%")  #en %
+
